a1e47d4138eac8410b6cbcf872243bdccf27c800
james <penakamaayo@gmail.com>
2015-08-28 12:28:36 +0800

    Setup session layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+
+
+  layout :choose_layout
+
+
+
+
+
+  protected
+
+  def choose_layout
+    return 'session' unless signed_in?
+  end
+
 end
