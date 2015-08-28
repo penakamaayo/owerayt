8bf95896847cffa2b2af67e7dacc4c6d878c71ad
james <penakamaayo@gmail.com>
2015-08-28 14:17:11 +0800

    Setup annotate gem

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  contact_id :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Contact < ActiveRecord::Base
 
   belongs_to :user
